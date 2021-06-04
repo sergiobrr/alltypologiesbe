e737548b2e3fa0f0aa450d952ed9d631c593d8cc
xpuser <lean@trovaprezzi.it>
2021-06-04 12:43:52 +0200

    Aggiunto .gitignore

---
+++ alltypologiesbe/base_model.py
@@ -0,0 +1,14 @@
+from django.db import models
+from uuid import uuid4
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateField(auto_now_add=True, editable=False, verbose_name='Creato il')
+    updated_at = models.DateField(auto_now=True, verbose_name='Modificato il')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'{self.__class__.__name__}-{self.id}'
